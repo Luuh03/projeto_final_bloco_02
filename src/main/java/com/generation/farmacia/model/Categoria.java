64ff1653ad48e9a97f92df58b05bd1728f8ffac2
Luan Queiroz <luan.queiroz2003@gmail.com>
2025-05-28 11:51:15 -0300

    :sparkles: Implementação das rotas CRUD de Categoria

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,43 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 100)
+	@NotBlank(message = "O atributo 'descrição' é obrigatório!")
+	@Size(min = 2, max = 50, message = "O atributo 'descrição' deve ter no mínimo 2 e no máximo 50 caractéres!")
+	@Pattern(regexp="^[^0-9].*", message = "O Atributo 'descrição' não pode ser composto somente por números!")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
